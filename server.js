b78c04bfe9885c70a8b3a66e0db92526b7328aaf
BPitts8019 <bradleyj.pitts@gmail.com>
2021-05-29 17:44:01 -0700

    Convert raw server into Express server Save raw server code

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const http = require("http");
+const express = require("express");
+const server = express();
 
-const server = http.createServer((req, res) => {
-   //use "req" to get information about the http request
-   //use "res" to send information back to the browser
-   res.statusCode = 200;
-   res.setHeader("Content-Type", "text/html");
-   res.write("<h1>Hello World</h1>");
-   res.end();
+server.get("/", (req, res) => {
+   res.json({ message: "Hello World!" });
 });
 
 server.listen(8080, () => {
