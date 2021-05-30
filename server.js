44ad8b8cc72237ff3fdfe13dee97c6ab1d87d5e1
BPitts8019 <bradleyj.pitts@gmail.com>
2021-05-29 18:14:19 -0700

    Add User endpoints

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const db = require("./database");
+
 const server = express();
 
 server.get("/", (req, res) => {
    res.json({ message: "Hello World!" });
+});
+
+server.get("/users", (req, res) => {
+   const users = db.getUsers();
+
+   res.json(users);
+});
+
+server.get("/users/:id", (req, res) => {
+   const user = db.getUserById(req.params.id);
+   if (user) {
+      res.json(user);
+      return;
+   }
+
+   res.status(404).json({ message: "User not found" });
 });
 
 server.listen(8080, () => {
